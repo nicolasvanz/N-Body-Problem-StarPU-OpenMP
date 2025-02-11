c7a550208604a9ab3f576a312907204a2484c3b5
nicolasvanz <nicolas.vanz@proton.me>
2025-02-11 17:16:05 -0300

    add bin2txt file
    this commit adds a simple script to convert bin files to txt. CPU and GPU functions use different functions to do the same calculation.
    this functions showed different precision, which causes solution and computed results to differ a little.
    the added script is for debugging purposes and aims to help if the computed results are really wrong or just present a small deviation

---
+++ src/bin2txt.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "include/body.h"
+
+#define NFILES 4
+
+int main(int argc, char **argv)
+{
+	const char *binFilenames[NFILES] = {
+		"debug/computed_pos_12",
+		"debug/solution_pos_12",
+		"debug/computed_vel_12",
+		"debug/solution_vel_12"};
+	const char *txtFilenames[NFILES] = {
+		"debug/computed_pos_12.txt",
+		"debug/solution_pos_12.txt",
+		"debug/computed_vel_12.txt",
+		"debug/solution_vel_12.txt"};
+
+	for (int i = 0; i < NFILES; i++)
+	{
+		FILE *binFile = fopen(binFilenames[i], "rb");
+		FILE *txtFile = fopen(txtFilenames[i], "w");
+
+		if (binFile == NULL || txtFile == NULL)
+		{
+			perror("Error opening file");
+			return 1;
+		}
+
+		Pos temp;
+		while (fread(&temp, sizeof(Pos), 1, binFile))
+		{ // Read struct from binary
+			fprintf(txtFile, "%f %f %f\n", temp.x, temp.y, temp.z);
+		}
+
+		fclose(binFile);
+		fclose(txtFile);
+	}
+
+	return 0;
+}
