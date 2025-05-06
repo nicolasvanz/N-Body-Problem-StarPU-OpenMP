031b002a01b62bd586e88b41d218baf26413b826
nicolasvanz <nicolas.vanz@proton.me>
2025-05-06 19:19:21 -0300

    gpu results

--- experiments.sh
+++ experiments.sh
@@ -28,10 +28,10 @@ function run_replications
 function starpu_gpu
 {
   (cd $dir_starpu && make clean && make)
-  for ((n=nbodies_initial_index; n<=nbodies_final_index; n++));
+  for n in 18 19
   do
     prefix="starpu_gpu-$n"
-    run="STARPU_NCPU=0 $dir_starpu/nbody $n"
+    run="$dir_starpu/nbody $n"
     run_replications "$run" "$prefix"
   done
 }
@@ -62,49 +62,4 @@ function starpu_cpu_gpu
   replace "$dir_starpu" "$starpu_parts_macro $experiments_starpu_parts" "$starpu_parts_default"
 }
 
-function openmp_cpu
-{
-  (cd $dir_openmp && make clean && make)
-  for ((n=nbodies_initial_index; n<=nbodies_final_index; n++));
-  do
-    prefix="openmp_cpu-$n"
-    run="OMP_NUM_THREADS=$experiments_omp_threads $dir_openmp/nbody $n"
-    run_replications "$run" "$prefix"
-  done
-}
-
-function openmp_gpu
-{
-  openmp_bodyforce_use_cpu_macro="#define BODYFORCE_USE_CPU"
-  openmp_bodyforce_use_cpu_default="$openmp_bodyforce_use_cpu_macro 1"
-  openmp_integratepositions_use_cpu_macro="#define INTEGRATEPOSITIONS_USE_CPU"
-  openmp_integratepositions_use_cpu_default="$openmp_integratepositions_use_cpu_macro 1"
-  replace "$dir_openmp" "$openmp_bodyforce_use_cpu_default" "$openmp_bodyforce_use_cpu_macro 0"
-  replace "$dir_openmp" "$openmp_integratepositions_use_cpu_default" "$openmp_integratepositions_use_cpu_macro 0"
-  (cd $dir_openmp && make clean && make)
-  for ((n=nbodies_initial_index; n<=nbodies_final_index; n++));
-  do
-    prefix="openmp_gpu-$n"
-    run="OMP_NUM_THREADS=$experiments_omp_threads $dir_openmp/nbody $n"
-    run_replications "$run" "$prefix"
-  done
-  replace "$dir_openmp" "$openmp_bodyforce_use_cpu_macro 0" "$openmp_bodyforce_use_cpu_default"
-  replace "$dir_openmp" "$openmp_integratepositions_use_cpu_macro 0" "$openmp_integratepositions_use_cpu_default" 
-}
-
-function openmp_cpu_gpu
-{
-  openmp_bodyforce_use_cpu_macro="#define BODYFORCE_USE_CPU"
-  openmp_bodyforce_use_cpu_default="$openmp_bodyforce_use_cpu_macro 1"
-  replace "$dir_openmp" "$openmp_bodyforce_use_cpu_default" "$openmp_bodyforce_use_cpu_macro 0"
-  (cd $dir_openmp && make clean && make)
-  for ((n=nbodies_initial_index; n<=nbodies_final_index; n++));
-  do
-    prefix="openmp_cpu_gpu-$n"
-    run="OMP_NUM_THREADS=$experiments_omp_threads $dir_openmp/nbody $n"
-    run_replications "$run" "$prefix"
-  done
-  replace "$dir_openmp" "$openmp_bodyforce_use_cpu_macro 0" "$openmp_bodyforce_use_cpu_default"
-}
-
-starpu_cpu
+starpu_gpu
